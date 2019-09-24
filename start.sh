614553a645bfb3cf3e2b4cacb7fc5be66369a50f
Mikael PANN <mikael.pann@stimio.fr>
2019-09-24 11:45:18 +0200

    [STIMIO] - Script to start

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#1) start prog
+python3 brother_ql_web.py
+
+
+
+#2) Use web interface  http://localhost:8013
+# print brother           : 192.168.110.191
+# printer fixe in pfsense : 192.168.110.5
